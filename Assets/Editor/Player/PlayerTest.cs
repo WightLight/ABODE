d4f64aa0f1c395ae86ce26c0b008eefa9197a3eb
auroratide <tf.auroratide@gmail.com>
2019-08-20 19:11:31 -0500

    Simple example of a test

---
+++ Assets/Editor/Player/PlayerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Abode.Tests
+{
+  public class PlayerTest
+  {
+    [Test]
+    public void GreetingShouldCostAp()
+    {
+      Player player = new Player();
+
+      player.Greet();
+
+      Assert.That(player.AP, Is.EqualTo(8));
+    }
+  }
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿namespace Abode
+{
+  public class Player
+  {
+    private int ap;
+    public int AP
+    {
+      get { return ap; }
+    }
+
+    public Player()
+    {
+      ap = 10;
+    }
+
+    public void Greet()
+    {
+      ap -= 2;
+    }
+  }
+}
