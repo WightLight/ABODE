7a406636452b6c1db101bc34f5559b85d8d50007
Brendan Phillips <chromewisp@gmail.com>
2019-08-25 18:46:24 -0500

    Beginning ARCore testing
    Added things for use with an AR plane object-placing tutorial. Renamed MainAR scene to BarnabyQuest.

---
+++ Assets/Scripts/ARController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARController : MonoBehaviour
+{
+    // List of planes that ARCore detected in the current frame.
+    private List<DetectedPlane> detectedPlanes = new List<DetectedPlane>();
+
+    public GameObject GridPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Check ARCore session status.
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        // Fill detectedPlanes list with planes that ARCore detected in the current frame.
+        Session.GetTrackables<DetectedPlane>(detectedPlanes, TrackableQueryFilter.New);
+
+        // Instantiate a grid for each DetectedPlane in detectedPlanes.
+        for(int i = 0; i < detectedPlanes.Count; i++)
+        {
+            GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+
+            // Set the position of grid and modify the vertices of the attached mesh
+            grid.GetComponent<GridVisualizer>().Initialize(detectedPlanes[i]);
+        }
+    }
+}
