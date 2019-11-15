f6fc02c1d65918efd7534fd2d3e9725f46821bf9
Brendan Phillips <chromewisp@gmail.com>
2019-11-15 15:20:25 -0600

    Action Buttons
    Added an Action Button prefab object with ActionButton script. These are made to perform an Action when the button is pressed.

---
+++ Assets/Scripts/ActionButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Abode;
+
+public class ActionButton : MonoBehaviour
+{
+    public TextMeshProUGUI LabelText;
+    public AudioSource Audio;
+    public ShootAction Action;
+
+    // Label button based on its action.
+    void Start()
+    {
+        // Hardcoded for now. Should later come from the action or elsewhere.
+        LabelText.text = "Shoot";
+    }
+
+    // Perform the button's associated action and play SFX.
+    // public void DoAction()
+    // {
+    //     Debug.Log("Do action!");
+    //     if(Action.IsApplicable())
+    //     {
+    //         Action.Act();
+    //         Audio.Play();
+    //     }
+    // }
+}
